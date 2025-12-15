6af515fd6ac04f4f964d32e2a894b87d2af5d951
root <surajphawade@gmail.com>
2025-12-15 11:50:54 +0100

    Infra files added

---
+++ Env-Infra/dev/providers.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.100"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "00ad737d-c579-441c-8386-0551f3fc4bf9"
+}
