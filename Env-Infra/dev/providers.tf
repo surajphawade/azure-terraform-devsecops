0cac2756de39514b489dd1f6c59a350eec522323
SurajPhawade <SurajPhawade@harshitasumit1gmail.onmicrosoft.com>
2025-12-16 16:52:50 +0000

    Updated providers.tf

--- Env-Infra/dev/providers.tf
+++ Env-Infra/dev/providers.tf
@@ -1,10 +1,8 @@
 terraform {
-  required_version = ">= 1.5.0"
-
   required_providers {
     azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.100"
+      source = "hashicorp/azurerm"
+      version = "4.8.0"
     }
   }
 }
